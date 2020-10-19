96205715173dbc9b4fe720358d1b0a5640b0ec3b
20taxitaxi00 <takushi0210@icloud.com>
2020-10-19 20:56:03 +0900

    宿題が終わったぞ

---
+++ app.js
@@ -0,0 +1,17 @@
+// 宿題
+// クラスのみんなの名簿を2次元配列で作ってきましょう
+// 配列の中に、連想配列のかたちです
+let studentlist = [
+	{name:'Rioさん', from:'沖縄', hobby:'バスケ'},
+	{name:'Ryuichiさん', from:'沖縄', hobby:'ゴルフ'},
+	{name:'Katumiさん', from:'沖縄', hobby:'ギター'},
+	{name:'Gokiさん', from:'沖縄', hobby:'サッカー'},
+	{name:'Maikoさん', from:'沖縄', hobby:'旅行'},
+	{name:'MIchiyoさん', from:'神奈川', hobby:'旅行'}
+];
+// リストの0番目の人の名前をconsoleで表示しましょう
+console.log(studentlist[0].name);
+// リストの3番めの人の出身地をconsoleで表示しましょう
+console.log(studentlist[3].from);
+// リストの4番目の人の趣味をconsoleで表示しましょう
+console.log(studentlist[4].hobby);
